43249775eed594a274d86f72e2f1c905f2377094
wfleaf <42556959+wfleaf@users.noreply.github.com>
2019-06-26 20:46:19 -0700

    Create Assignment_1_6.py

---
+++ Assignment_1_6.py
@@ -0,0 +1,7 @@
+#Assignment 1.6: Write a program that displays the result of
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+
+seriesSum = 0
+for num in range (1,10):
+    seriesSum = seriesSum + num
+print (seriesSum)
